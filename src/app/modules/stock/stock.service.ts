6d1650d99ede8bc5e5858ace0f195e8334e98deb
Shourav Paul <paul.shourav01@gmail.com>
2025-01-15 02:37:41 +0600

    Stock updated

--- src/app/modules/stock/stock.service.ts
+++ src/app/modules/stock/stock.service.ts
@@ -32,7 +32,6 @@ const createStockDB = async (
   if (Array.isArray(files) && files.length > 0) {
     payload.images = files.map((file) => file.path);
   }
-  
 
   const result = await Stock.findByIdAndUpdate(
     stockId,
@@ -43,49 +42,59 @@ const createStockDB = async (
 };
 
 const getAllStocksDB = async (query: Record<string, unknown>) => {
-  
-
-  console.log(query,"query")
+  console.log(query, "query");
   const detailConditions: any[] = [];
-
+  const page = parseInt(query.page as string) || 1; // Default start index
+  const limit = parseInt(query.limit as string) || 5;
   // Add approvalStatus condition if provided
   if (query?.approvalStatus !== undefined) {
     detailConditions.push({
-      $eq: ["$$detail.approvalDetails.isApproved", Boolean(query?.approvalStatus)],
+      $eq: [
+        "$$detail.approvalDetails.isApproved",
+        JSON.parse(query?.approvalStatus as string),
+      ],
     });
   }
 
   // Add dateRange conditions if provided
-  if (query?.startDate) {
-    detailConditions.push({
-      $gte: ["$$detail.createdAt", new Date(query?.startDate as string)],
-    });
-  }
-  if (query?.endDate) {
-    detailConditions.push({
-      $lte: ["$$detail.createdAt", new Date(query?.endDate as string)],
-    });
+  if (query?.startDate && query?.endDate) {
+    console.log(
+      new Date(query?.startDate as string),
+      new Date(query?.endDate as string),
+      "date"
+    );
+    detailConditions.push(
+      { $gte: ["$$detail.createdAt", new Date(query?.startDate as string)] },
+      { $lte: ["$$detail.createdAt", new Date(query?.endDate as string)] }
+    );
   }
 
- 
+  console.log(detailConditions, "detailsCondition");
   const pipeline = [
-    { $match: { _id: new Types.ObjectId(query._id as string) } }, 
+    { $match: { _id: new Types.ObjectId(query._id as string) } },
     {
       $project: {
         _id: 1,
         quantityDetails: 1,
         codeDetails: 1,
         details: {
-          $filter: {
-            input: "$details", 
-            as: "detail", 
-            cond: detailConditions.length > 0 
-            ? { $and: detailConditions } 
-            : { $literal: true }
-          }
-        }
-      }
-    }
+          $slice: [
+            {
+              $filter: {
+                input: "$details",
+                as: "detail",
+                cond:
+                  detailConditions.length > 0
+                    ? { $and: detailConditions }
+                    : { $literal: true },
+              },
+            },
+            0, // Start index for pagination
+            limit*page, // Number of items to return
+          ],
+        },
+      },
+    },
   ];
 
   const stocks = await Stock.aggregate(pipeline);
@@ -99,13 +108,12 @@ const updateStockApprovedStatusDB = async (
   stockId: string,
   stockDetailsId: string
 ) => {
-
   // Check if Stock exists
   const isStockExists = await Stock.findOne(
     { _id: stockId, "details._id": stockDetailsId },
     {
       "details.$": 1,
-      quantityDetails: 1, 
+      quantityDetails: 1,
       codeDetails: 1,
     }
   );
@@ -146,7 +154,7 @@ const updateStockApprovedStatusDB = async (
       "details.$.approvalDetails.approvedDate": new Date(),
     },
   };
-  console.log(isStockExists);
+ 
   // Update Stock approval details
   const result = await Stock.findOneAndUpdate(
     { _id: stockId, "details._id": stockDetailsId },
@@ -155,63 +163,66 @@ const updateStockApprovedStatusDB = async (
   );
   return result;
 };
-const getSingleStockDB=async(stockId: string,
-  stockDetailsId: string)=>{
-    const isStockExists = await Stock.findOne(
-      { _id: stockId, "details._id": stockDetailsId },
-      {
-        "details.$": 1
-      }
+const getSingleStockDB = async (stockId: string, stockDetailsId: string) => {
+  const isStockExists = await Stock.findOne(
+    { _id: stockId, "details._id": stockDetailsId },
+    {
+      "details.$": 1,
+    }
+  );
+  if (!isStockExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "stockError",
+      "Stock doesn't exist."
     );
-    if (!isStockExists) {
-      throw new AppError(
-        httpStatus.NOT_FOUND,
-        "stockError",
-        "Stock doesn't exist."
+  }
+  return isStockExists.details[0];
+};
+const updateStockDB = async (
+  stockId: string,
+  stockDetailsId: string,
+  files: any,
+  payload: Partial<TStockDetail>
+) => {
+  const isStockExists = await Stock.findOne(
+    { _id: stockId, "details._id": stockDetailsId },
+    {
+      "details.$": 1,
+    }
+  );
+  if (!isStockExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "stockError",
+      "Stock doesn't exist."
+    );
+  }
+  if (Array.isArray(files) && files.length > 0) {
+    if (isStockExists?.details[0]?.images.length > 0) {
+      isStockExists?.details[0]?.images?.forEach(
+        async (image) => await deleteFileFromCloudinary(image)
       );
     }
-    return isStockExists.details[0]
-}
-const updateStockDB=async(stockId: string,
-  stockDetailsId: string,files:any,payload:Partial<TStockDetail>)=>{
-    const isStockExists = await Stock.findOne(
-      { _id: stockId, "details._id": stockDetailsId },
-      {
-        "details.$": 1
-      }
-    );
-    if (!isStockExists) {
-      throw new AppError(
-        httpStatus.NOT_FOUND,
-        "stockError",
-        "Stock doesn't exist."
-      );
-    }
-    if (Array.isArray(files) && files.length > 0) {
-      if (isStockExists?.details[0]?.images.length>0) {
-        
-        isStockExists?.details[0]?.images?.forEach(async(image)=> await deleteFileFromCloudinary(image))
-      }
-      payload.images = files.map((file) => file.path);
-    }
-    const result = await Stock.findOneAndUpdate(
-      { _id: stockId, "details._id": stockDetailsId },
-    {  $set: {
-        
+    payload.images = files.map((file) => file.path);
+  }
+  const result = await Stock.findOneAndUpdate(
+    { _id: stockId, "details._id": stockDetailsId },
+    {
+      $set: {
         "details.$.quantity": payload.quantity,
         "details.$.images": payload.images,
         "details.$.description": payload.description,
-        
-      }},
-      { new: true }
-    );
-    return result;
-    
-}
+      },
+    },
+    { new: true }
+  );
+  return result;
+};
 export const StockService = {
   createStockDB,
   getAllStocksDB,
   updateStockApprovedStatusDB,
   getSingleStockDB,
-  updateStockDB
+  updateStockDB,
 };
